2d37981361e58154ee70fe55a54a509a0ff8befa
Edmarcio Carlos Fonseca Junior <Edmarcio.junior@outlook.com>
2025-01-09 15:45:23 -0300

    Dia01 em PY
    Primeiro dia descobrindo mais sobre Python.

---
+++ Python/Dia01.py
@@ -0,0 +1,26 @@
+#region(Print e Comentarios)
+# Primeiro comentario em codigo. - (Comentario por linha)
+
+''' (Comentario Linhas multiplas)
+Comentario por multiplas linhas.
+'''
+
+print('E isso ae bora aprender');
+#endregion
+
+#region(Variaveis)
+
+Nome = 'Edmarcio' # string -Armazera caracteres.
+Idade = '25' # int - Permite fazer calculos e armazenar numeros inteiros.
+Peso = '74,50' # float - Permite fazer calculos e armazenar numeros decimais.
+Existe =  True; # bool - Permite fazer validação de verdadeiro ou falso.
+Visivel = None; # nome - Ausensia de valor.
+
+print(Nome);
+print(Idade);
+print(Peso);
+print(Existe);
+print(Visivel);
+
+
+#endregion
